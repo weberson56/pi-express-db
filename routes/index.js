99c14e573db82e09cc4d06cd9ae4979dde1c5eb8
weberson <bersoncleiton12@gmail.com>
2024-06-25 17:21:41 -0300

    hora e data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  router.get('/', function(req, res, next,) {
+    const now = new Date()
+    const name = {
+    title: "Express",
+    name: "weberson",
+    data: [now.getDate(), now.getMonth(), now.getFullYear(),s()].join(' \\ '),
+    hora: [now.getHours(),now.getMinutes(), now.getSecond].join(' \\ ')
+  }
+  res.render('index', name);
+  });
 
 module.exports = router;
