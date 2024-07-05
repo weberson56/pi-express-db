17fb65b08349e529a9e37adceb0941e144665ba7
weberson <bersoncleiton12@gmail.com>
2024-07-05 16:31:25 -0300

    novos parametros

--- routes/alunos.js
+++ routes/alunos.js
@@ -12,6 +12,36 @@ router.get('/', function(req, res, next) {
     res.render('list',data)
 });
 
+router.get('/new', function(_req, res, next) {
+    res.render('form', {title: 'Novo aluno', buttonText: 'Adicionar aluno'});
+});
+
+router.post('/creat', function(req, res, next) {
+    const novoAluno = req.body;
+    const matricula = novoAluno.matricula;
+
+
+    alunos.content[matricula] = {
+        ...novoAluno,
+        matricula: Number(matricula),
+    };
+
+    res.redirect('/alunos');
+});
+
+
+router.put('/', function (req, res, next) {
+    
+    res.send(rq.body);
+});
+
+
+router.delete('/', function (req, res, next) {
+    
+    res.send(rq.body);
+});
+
+
 router.get('/:matricula', function(req, res, next) {
 
     const {matricula} =  req.params;
@@ -21,8 +51,15 @@ router.get('/:matricula', function(req, res, next) {
 
     res.render('card',{title:'Detalhe dos alunos', aluno})
 });
+
+router.get('/edit/:matricula', function(req, res, next) {
+
+    const {matricula} =  req.params;
+
+    const aluno = alunos.content[matricula];
+
+    res.render('form', {title: 'Editar aluno', buttonText: 'Salvar Alterações', aluno});
+});
+
 module.exports = router;
 
-router.get('/new', function(_req, res, next){
-    res 
-})
